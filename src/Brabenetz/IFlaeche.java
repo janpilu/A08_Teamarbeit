af8dec918528988269595124a3f6f23a088ae40f
Michael Ebenstein <mebenstein@student.tgm.ac.at>
2016-11-10 10:18:59 +0100

    Added Comment

--- src/Brabenetz/IFlaeche.java
+++ src/Brabenetz/IFlaeche.java
@@ -1,5 +1,15 @@
 package Brabenetz;
 
+/**
+ * 
+ * @author Michael Ebenstein
+ * Interface for area
+ */
 public interface IFlaeche {
+	
+	/**
+	 * Gets the area of an 2D object
+	 * @return Area of the object
+	 */
 	public double getFlaeche();
 }
