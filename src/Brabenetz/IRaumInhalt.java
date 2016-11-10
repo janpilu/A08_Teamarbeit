e26455f5bc0669d2ac253ab3d629ab0977ad34cf
Alexander Strasser <astrasser@tgm.ac.at>
2016-11-10 10:26:50 +0100

    Add comments of IRauminhalt

--- src/Brabenetz/IRaumInhalt.java
+++ src/Brabenetz/IRaumInhalt.java
@@ -1,5 +1,17 @@
 package Brabenetz;
 
+/**
+ * 
+ * @author Alexander Strasser
+ * @version 2016-10-11
+ *
+ */
 public interface IRaumInhalt {
+	
+	/**
+	 * Calculates the volume of the 3D-object
+	 * 
+	 * @return the calculated volume
+	 */
 	double getRaumInhalt();
 }
