f5438bbec9abeae342a2422f70a1a49bb49440a0
Michael Ebenstein <mebenstein@student.tgm.ac.at>
2016-11-10 10:47:22 +0100

    Changed data to data2D and made it protected

--- src/Brabenetz/Object2D.java
+++ src/Brabenetz/Object2D.java
@@ -9,24 +9,24 @@ package Brabenetz;
  *
  */
 public class Object2D implements IFlaeche{
-	private double data[];
+	protected double data2D[];
 	
 	public Object2D(double radius){
-		data = new double[1];
-		data[0] = radius;
+		data2D = new double[1];
+		data2D[0] = radius;
 	}
 	
 	public Object2D(double width,double height){
-		data = new double[2];
-		data[0] = width;
-		data[1] = height;
+		data2D = new double[2];
+		data2D[0] = width;
+		data2D[1] = height;
 	}
 	
 	public Object2D(double a,double b,double c){
-		data = new double[3];
-		data[0] = a;
-		data[1] = b;
-		data[2] = c;
+		data2D = new double[3];
+		data2D[0] = a;
+		data2D[1] = b;
+		data2D[2] = c;
 	}
 	
 	@Override
@@ -34,9 +34,12 @@ public class Object2D implements IFlaeche{
 	 * @see IFlaeche.java
 	 */
 	public double getFlaeche() {
-		if(data.length == 1)return data[0]*data[0]*Math.PI;
-		else if(data.length == 2)return data[0]*data[1];
-		else if(data.length == 3)return 0;
+		if(data2D.length == 1)return data2D[0]*data2D[0]*Math.PI;
+		else if(data2D.length == 2)return data2D[0]*data2D[1];
+		else if(data2D.length == 3){
+			double s = (data2D[0]+data2D[1]+data2D[2])/2;
+			return Math.sqrt(s*(s-data2D[0])*(s-data2D[1])*(s-data2D[2]));
+		}
 		return 0;
 	}
 
