e0a25b2ba5ec9e3e5cbd6af99b6cf47cb808c477
Peter Fuchs <pedda.fux@gmail.com>
2016-11-10 10:54:12 +0100

    Added commentaries and constructor

--- src/Brabenetz/Numbers.java
+++ src/Brabenetz/Numbers.java
@@ -1,34 +1,54 @@
-
+package Brabenetz;
 
 import java.util.LinkedList;
 
+/**
+ * This Object principally has a double-Collection and can return<br>
+ * the largest number, the smallest number and the sum of all the numbers in the collection.<br>
+ * 
+ * @author Peter Fuchs
+ * @version 2016-11-10
+ */
 public class Numbers implements IZahlen {
 	
 	private LinkedList<Double> numbers;
 	
+	/**
+	 * Constructor
+	 */
 	public Numbers() {
 		this.numbers = new LinkedList<Double>();
 	}
-	
-	@Override
+
+	/**
+	 * @return the sum of the numbers in the collection
+	 */
 	public double getSumme() {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
-	@Override
+	/**
+	 * @return the minimum number of the collection
+	 */
 	public double getMinimum() {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
-	@Override
+	/**
+	 * @return the maximum number of the collection
+	 */
 	public double getMaximum() {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
-	@Override
+	/**
+	 * Adds a number to the collection
+	 * 
+	 * @param value the value to be added
+	 */
 	public void add(double value) {
 		
 	}
