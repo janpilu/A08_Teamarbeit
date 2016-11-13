7dfe216e6a2c5b3c67a57f08ac2f3c9f8109e7e8
Oskar Koiner <okoiner@student.tgm.ac.at>
2016-11-13 22:21:09 +0100

    Added Testklasse

---
+++ src/Brabenetz/Testklasse.java
@@ -0,0 +1,19 @@
+package Brabenetz;
+
+
+/**
+ * @author Oskar Koiner
+ * @version 13.11.2016
+ *
+ */
+public class Testklasse {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
