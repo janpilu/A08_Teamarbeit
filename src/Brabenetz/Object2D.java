26994e68260e3e91dfaebbe8591f6fc43fb7bf51
Michael Ebenstein <michael@ebenstein.eu>
2016-11-15 18:12:47 +0100

    Changed getFlaeche
    Fixed bug in getFlaeche where the value would be Nan

--- src/Brabenetz/Object2D.java
+++ src/Brabenetz/Object2D.java
@@ -65,7 +65,7 @@ public class Object2D implements IFlaeche{
 		else if(data2D.length == 2)return data2D[0]*data2D[1];
 		else if(data2D.length == 3){
 			double s = (data2D[0]+data2D[1]+data2D[2])/2;
-			return Math.sqrt(s*(s-data2D[0])*(s-data2D[1])*(s-data2D[2]));
+			return Math.sqrt(abs(s*(s-data2D[0])*(s-data2D[1])*(s-data2D[2])));
 		}
 		return 0;
 	}
